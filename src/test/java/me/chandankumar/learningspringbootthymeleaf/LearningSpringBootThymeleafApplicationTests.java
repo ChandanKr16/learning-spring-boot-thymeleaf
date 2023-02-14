766cd49d74b9e269e48c02fd1c21695944dde90d
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-14 21:35:35 +0530

    Created empty project

---
+++ src/test/java/me/chandankumar/learningspringbootthymeleaf/LearningSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chandankumar.learningspringbootthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
